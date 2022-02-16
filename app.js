29a120c69d548ec3ef629f9417338336b9c974b2
ouda21 <ouda.wycliffe@gmail.com>
2022-02-16 12:37:43 +0300

    Second server configuration

--- app.js
+++ app.js
@@ -3,11 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+
+dotenv.config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => console.log("DB Connection Successful"))
+    .catch((err) => {
+      console.error(err);
+    });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +40,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
